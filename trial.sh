b527c7f75e67204aa495e725011064dce329cc2a
doreimon1719 <30547401+doreimon1719@users.noreply.github.com>
2017-07-29 23:06:52 -0700

    Update trial.sh

--- trial.sh
+++ trial.sh
@@ -5,9 +5,9 @@
 #
 cd
 clear
-echo "==================================================================="
-echo -e "          TRIAL USER MENU | MENU SCRIPT BY syahz86           "
-echo "==================================================================="
+#echo "==================================================================="
+#echo -e "          TRIAL USER MENU | MENU SCRIPT BY syahz86           "
+#echo "==================================================================="
 echo ""
 IP=`dig +short myip.opendns.com @resolver1.opendns.com`
 
@@ -18,15 +18,15 @@ Pass=`</dev/urandom tr -dc a-f0-9 | head -c3`
 useradd -e `date -d "$hari days" +"%Y-%m-%d"` -s /bin/false -M $Login
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
-echo -e "====Trial akaun SSH===="
-echo -e "Host: $IP" 
-echo -e "Port OpenSSH: 22,143"
-echo -e "Port Dropbear: 80,443"
-echo -e "Port Squid: 8080,3128"
-echo -e "Config OpenVPN (TCP 1194): http://$IP:81/client.ovpn"
-echo -e "Username: $Login"
-echo -e "Password: $Pass\n"
-echo "==================================================================="
-echo -e "              SCRIPT BY syahz86 | GollumVPN              "
-echo "==================================================================="
-echo ""
+#echo -e "====Trial akaun SSH===="
+#echo -e "Host: $IP" 
+#echo -e "Port OpenSSH: 22,143"
+#echo -e "Port Dropbear: 80,443"
+#echo -e "Port Squid: 8080,3128"
+#echo -e "Config OpenVPN (TCP 1194): http://$IP:81/client.ovpn"
+#echo -e "Username: $Login"
+#echo -e "Password: $Pass\n"
+#echo "==================================================================="
+#echo -e "              SCRIPT BY syahz86 | GollumVPN              "
+#echo "==================================================================="
+#echo ""
