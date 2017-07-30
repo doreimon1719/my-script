58d61891d7af5c0bc2ba52d601ec7e88d325401c
doreimon1719 <30547401+doreimon1719@users.noreply.github.com>
2017-07-30 21:31:51 +0800

    Update Unblockuser.sh

--- Unblockuser.sh
+++ Unblockuser.sh
@@ -6,13 +6,13 @@
 cd
 clear
 echo "==================================================================="
-echo -e "         UNBLOCK USER MENU | MENU SCRIPT BY syahz86          "
+echo -e "         UNBLOCK USER MENU |           "
 echo "==================================================================="
 echo ""
 read -p "What username you to unblock: " Nama
 passwd -u $Nama
 echo ""
-echo "==================================================================="
-echo -e "              SCRIPT BY syahz86 | GollumVPN              "
+#echo "==================================================================="
+#echo -e "              SCRIPT BY syahz86 | GollumVPN              "
 echo "==================================================================="
 echo ""
