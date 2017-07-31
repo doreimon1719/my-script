31a2378eb013e30204178fca1df09468716d9a92
doreimon1719 <30547401+doreimon1719@users.noreply.github.com>
2017-07-31 08:25:44 -0700

    Update user-add.sh

--- user-add.sh
+++ user-add.sh
@@ -21,7 +21,7 @@ else
 	today="$(date +"%Y-%m-%d")"
 	expire=$(date -d "$AKTIF days" +"%Y-%m-%d")
 	## useradd -M -N -s /bin/false -e $expire $username
-	echo $username:$password | chpasswd
+	##echo $username:$password | chpasswd
 	echo ""
 	echo "-------------------------------------------------------------------"
 	echo "Data Login:"
