3455d942c299297d6a6f6edb306df3b04644f909
doreimon1719 <30547401+doreimon1719@users.noreply.github.com>
2017-07-31 07:46:28 -0700

    Update user-add.sh

--- user-add.sh
+++ user-add.sh
@@ -20,7 +20,7 @@ else
 	read -p "How many day [$username] active: " AKTIF
 	today="$(date +"%Y-%m-%d")"
 	expire=$(date -d "$AKTIF days" +"%Y-%m-%d")
-	useradd -M -N -s /bin/false -e $expire $username
+	## useradd -M -N -s /bin/false -e $expire $username
 	echo $username:$password | chpasswd
 	echo ""
 	echo "-------------------------------------------------------------------"
