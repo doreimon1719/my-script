289c6901cbdb7c4f4cff67a08e0467f9892decff
doreimon1719 <30547401+doreimon1719@users.noreply.github.com>
2017-07-31 07:20:23 -0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -17,26 +17,25 @@ echo -e "\033[1;31m  6\033[0m) Unlock user account          |   (Unblock user) "
 echo -e "\033[1;31m  8\033[0m) User account list            |   (all users' list) "       ##4
 echo -e "\033[1;31m  9\033[0m) User expire list             |   (user expire list)  "     ##10
 echo -e "\033[1;31m 10\033[0m) Change user password         |   (change pass of user) "   ##5
-
-echo -e "\033[1;31m  3\033[0m) Generate user account        |   (fast create user-pass) "
-
-
+echo -e "\033[1;31m 11\033[0m) Generate user account         |   (fast create user-pass) " ##3
+echo -e "\033[1;31m 12\033[0m) Manual Kill multilogin               |  (user limit to 1) "  ##14
 
 
-echo -e "\033[1;31m 8\033[0m) Sekat akaun yang sudah expire        | \033[1;32mDisable user expire\033[0m"
 
-echo -e "\033[1;31m10\033[0m) Memadam akaun yang sudah expire      | \033[1;32mDelete user expire\033[0m"
+echo -e "\033[1;31m \033[0m) Disable user expire            |   ( user no expiry) " ##8
+
+echo -e "\033[1;31m \033[0m) Delete user expire            |   ( Delete expired user/s) " ##10
 
 
 echo -e "\033[1;31m13\033[0m) Senarai akaun yang sedang login      | \033[1;32mUser active list\033[0m"
-echo -e "\033[1;31m14\033[0m) Sekat multilogin (manual)            | \033[1;32mKill multilogin manual\033[0m"
+
 echo -e "\033[1;31m15\033[0m) Jumlah RAM yang sedang digunakan     | \033[1;32mTotal used RAM\033[0m"
 echo -e "\033[1;31m16\033[0m) Restart semua servis                 | \033[1;32mRestart all service\033[0m"
 echo -e "\033[1;31m17\033[0m) Speedtest VPS                        | \033[1;32mSpeedtest --share\033[0m"
 echo -e "\033[1;31m18\033[0m) Benchmark                            | \033[1;32mBenchmark\033[0m"
 echo -e "\033[1;31m19\033[0m) Maklumat Sistem VPS                  | \033[1;32mVPS system info\033[0m"
-echo -e "\033[1;31m20\033[0m) Info script auto install             | \033[1;32mAuto install script info\033[0m"
-echo -e "\033[1;31m21\033[0m) Reboot server                        | \033[1;32mReboot server\033[0m"
+echo -e "\033[1;31m20\033[0m) Info script auto install             | (Auto install script info) "
+echo -e "\033[1;31m21\033[0m) Reboot server                        | (Reboot server) "
 echo -e "==================================================================="
 echo -e "              SCRIPT BY syahz86 | GollumVPN              "
 echo -e "==================================================================="
