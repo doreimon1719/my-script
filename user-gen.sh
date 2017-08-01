fe435f5afd0df590057a1b7bb8e3e3f78a5842fa
doreimon1719 <30547401+doreimon1719@users.noreply.github.com>
2017-08-01 05:39:36 -0700

    Update user-gen.sh

--- user-gen.sh
+++ user-gen.sh
@@ -13,8 +13,8 @@ echo "==================================================================="
 echo -e "      GENERATE AKAUN PELANGGAN | MENU SCRIPT BY syahz86      "
 echo "==================================================================="
 echo -e ""
-read -p "Berapa jumlah akaun yang akan dibuat: " JUMLAH
-read -p "Berapa hari akaun aktif: " AKTIF
+read -p "How many accounts will be created: " JUMLAH
+read -p "How many days are active accounts: " AKTIF
 today="$(date +"%Y-%m-%d")"
 expire=$(date -d "$AKTIF days" +"%Y-%m-%d")
 echo ""
@@ -28,7 +28,7 @@ echo "Squid Proxy: 80, 8080, 3128"
 echo "OpenVPN Config: http://$MYIP:81/client.ovpn"
 for (( i=1; i <= $JUMLAH; i++ ))
 do
-	username=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1`;
+	username=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 7 | head -n 1`;
 	useradd -M -N -s /bin/false -e $expire $username
 	#password=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1`;
 	echo $username:$username | chpasswd
@@ -36,7 +36,7 @@ do
 	echo "$i. Username/Password: $username"
 done
 
-echo "Aktif sehingga: $(date -d "$AKTIF days" +"%d-%m-%Y")"
+echo "Active up to: $(date -d "$AKTIF days" +"%d-%m-%Y")"
 echo -e ""
 echo "==================================================================="
 echo -e "              SCRIPT BY syahz86 | GollumVPN              "
