6d8702107c4cf2c6f9c831e208fce0121ef80f77
doreimon1719 <30547401+doreimon1719@users.noreply.github.com>
2017-08-05 19:06:27 -0700

    Update user-gen.sh

--- user-gen.sh
+++ user-gen.sh
@@ -1,7 +1,6 @@
 #
 #!/bin/bash
-#Script generate akaun
-#Script by syahz86
+
 #
 export DEBIAN_FRONTEND=noninteractive
 OS=`uname -m`;
@@ -28,9 +27,9 @@ echo "Squid Proxy: 80, 8080, 3128"
 echo "OpenVPN Config: http://$MYIP:81/client.ovpn"
 for (( i=1; i <= $JUMLAH; i++ ))
 do
-	username=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 7 | head -n 1`;
+	username=`cat /dev/urandom | trial -dc 'a-zA-Z0-9' | fold -w 7 | head -n 1`;
 	useradd -M -N -s /bin/false -e $expire $username
-	#password=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1`;
+	password=`cat /dev/urandom | dc 'a-zA-Z0-9' | fold -w 3 | head -n 1`;
 	echo $username:$username | chpasswd
 	
 	echo "$i. Username/Password: $username"
